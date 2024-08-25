e16bedba64c06e81e3f1dbd8e72c2ed377ee8288
ASHWIN <meashwinclarence@gmail.com>
2024-08-25 20:27:28 +0530

    fea: type into object

--- src/Components/TodoSearch/TodoSearch.tsx
+++ src/Components/TodoSearch/TodoSearch.tsx
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 import "./TodoSearch.css";
 
+type TodoType = {
+  id: string;
+  content: string;
+  isComplete: boolean;
+};
+
 const TodoSearch = () => {
   const [todoInput, setTodoInput] = useState<string>("");
-  const [listArray, setListArray] = useState<string[]>([]);
+  const [listArray, setListArray] = useState<TodoType[]>([]);
 
   const handleSubmitTodo = (event: React.FormEvent) => {
-    event.preventDefault();
-    setListArray((prev) => [...prev, todoInput]);
-    setTodoInput("");
+      event.preventDefault();
+      
+      if (todoInput.trim() !== '') {
+          
+          setListArray((prev) => [
+              ...prev,
+              { id: Date.now().toString(), content: todoInput, isComplete: false },
+            ]);
+            setTodoInput("");
+        }
+  };
+
+  const handleRemoveTodo = (id: string) => {
+    setListArray((prev) => prev.filter((ele) => ele.id !== id));
     };
     
 
-    const handleRemoveTodo = (index:number) => {
-        setListArray((prev)=>prev.filter((ele,ind)=>ind!==index));
+    // function to complete the todo
+    const handleCompleteTodo = (id: string) => {
+        setListArray((prev) => (
+            prev.map((items) => items.id===id?{...items,isComplete:!items.isComplete}:items)
+        ))
     }
 
   return (
@@ -29,14 +49,17 @@ const TodoSearch = () => {
         <button type="submit" className="bg-red-600 px-5 py-2 rounded-md">
           Add
         </button>
-          </form>
-          
-          {/* this section is the list of todo is displayed */}
+      </form>
+
+      {/* this section is the list of todo is displayed */}
       <div className="flex flex-col gap-5">
-        {listArray.map((listElement, index) => (
-          <div className="flex items-center gap-4" key={index}>
-            <input type="checkbox" name="todo-list" className="list-items" />
-            <label htmlFor="list-items">{listElement}</label>
+        {listArray.map((listElement,index) => (
+          <div className="flex items-center justify-between w-[20vw]" key={listElement.id}>
+                <div className="flex items-center gap-2">
+                    <h2>{index+1}:</h2>
+              {/* <input type="checkbox" name="todo-list" className="list-items me-3" onChange={()=>handleCompleteTodo(listElement.id)} /> */}
+              <label htmlFor="list-items">{listElement.content}</label>
+            </div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -47,8 +70,8 @@ const TodoSearch = () => {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-                    className="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-x"
-                    onClick={()=>handleRemoveTodo(index)}
+              className="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-x text-red-600"
+              onClick={() => handleRemoveTodo(listElement.id)}
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
               <path d="M10 10l4 4m0 -4l-4 4" />
